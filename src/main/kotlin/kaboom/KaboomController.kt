035fb73cde8bad337e73dc9b079c45ee38a15121
Trevor Whitney <twhitney@pivotal.io>
2017-09-25 17:01:49 -0600

    make actuator metrics work in non-ssl cf deploy
    Signed-off-by: Todd Persen <tpersen@pivotal.io>

--- src/main/kotlin/kaboom/KaboomController.kt
+++ src/main/kotlin/kaboom/KaboomController.kt
@@ -1,15 +1,20 @@
 package kaboom
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class OverflowHeapController(val kaboomService: KaboomService) {
+    private final val logger = LoggerFactory.getLogger(OverflowHeapController::class.java)
     val objectCollections: MutableList<LargeObject?> = mutableListOf<LargeObject?>()
 
     @RequestMapping("/kaboom")
     fun kaboom(@RequestParam("size") size: Int): Int {
+        if (logger.isDebugEnabled) {
+            logger.debug("Processing kaboom request for size: $size")
+        }
         objectCollections.addAll(kaboomService.kaboom(size))
         return objectCollections.size
     }
